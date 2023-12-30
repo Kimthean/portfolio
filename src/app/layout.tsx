ee8f7ff86b40872c66a673247677e9386e9f3967
Thean <theantian089@gmail.com>
2023-12-30 21:49:38 +0700

    add og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,19 @@ import Navbar from "@/components/Navbar";
 import styles from "./RootLayout.module.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://www.thean.codes"),
   title: "Thean's Portfolio",
   description: "My personal portfolio website",
+  openGraph: {
+    images: [
+      {
+        url: "/assets/thumbnail.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Porfolio",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
